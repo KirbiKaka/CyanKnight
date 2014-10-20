9e95926be99580eca7fcb1e2085c371eef2a8e25
James Liao <jwindchime@berkeley.edu>
2014-10-19 22:21:30 -0700

    Re-added Paradigm Controls

---
+++ Assets/ParadigmControls.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var num_paradigms = 3;
+var curr_paradigms = new Array(3);
+var delay = 5;
+
+function Start () {
+	curr_paradigms = ["q", "w", "e"];
+}
+
+function Update () {
+	if (Input.GetKeyDown("q")) updateList("q");
+	else if (Input.GetKeyDown("w")) updateList("w");
+	else if (Input.GetKeyDown("e")) updateList("e");
+	
+	Debug.Log(curr_paradigms);
+}
+
+function updateList(newParadigm) {
+	curr_paradigms[0] = curr_paradigms[1];
+	curr_paradigms[1] = curr_paradigms[2];
+	curr_paradigms[2] = newParadigm;
+		
+	yield WaitForSeconds (5);
+	delay = delay;
+}
