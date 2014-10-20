9bac23f625b3d5566b9ad2d180ac99d4b5c9e942
James Liao <jwindchime@berkeley.edu>
2014-10-19 23:00:02 -0700

    Added faulty delay in Paradigm Controls

--- Assets/ParadigmControls.js
+++ Assets/ParadigmControls.js
@@ -3,15 +3,36 @@
 var num_paradigms = 3;
 var curr_paradigms = new Array(3);
 var delay = 5;
+var press_time = 0;
+var curr_time = 0;
+var pressable = true;
 
 function Start () {
 	curr_paradigms = ["q", "w", "e"];
 }
 
 function Update () {
-	if (Input.GetKeyDown("q")) updateList("q");
-	else if (Input.GetKeyDown("w")) updateList("w");
-	else if (Input.GetKeyDown("e")) updateList("e");
+	if (pressable) {
+		if (Input.GetKeyDown("q")) {
+			updateList("q");
+			press_time = Date.Now.Ticks;
+			pressable = false;
+		}
+		else if (Input.GetKeyDown("w")) {
+			updateList("w");
+			press_time = Date.Now.Ticks;
+			pressable = false;
+		}
+		else if (Input.GetKeyDown("e")) {
+			updateList("e");
+			press_time = Date.Now.Ticks;
+			pressable = false;
+		}
+	}
+	
+	curr_time = Date.Now.Ticks;
+	Debug.Log(curr_time - press_time);
+	if (!pressable && (curr_time - press_time >= 5000)) pressable = true;
 	
 	Debug.Log(curr_paradigms);
 }
@@ -20,7 +41,4 @@ function updateList(newParadigm) {
 	curr_paradigms[0] = curr_paradigms[1];
 	curr_paradigms[1] = curr_paradigms[2];
 	curr_paradigms[2] = newParadigm;
-		
-	yield WaitForSeconds (5);
-	delay = delay;
 }
