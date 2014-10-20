42c2928157cf14e0e75dd856fdb19c2af5582b31
KirbiKaka <calvinlu@berkeley.edu>
2014-10-19 21:04:21 -0700

    Unity Tutorial Stuff Basics

---
+++ Assets/MoveControls.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var speed = 50;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if (Input.GetKeyDown(KeyCode.LeftArrow))
+		transform.Translate (Vector3 (-1, 0, 0) * Time.deltaTime * speed);
+
+}
+
+function OnCollisionEnter2D(coll: Collision2D) {
+	if (coll.gameObject.tag == "Wall") {
+		coll.gameObject.SendMessage("Stop", 10);
+	}
+}
