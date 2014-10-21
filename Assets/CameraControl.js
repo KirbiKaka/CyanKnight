4e37539a7addfa26004aee591725b394de859773
KirbiKaka <calvinlu@berkeley.edu>
2014-10-21 03:43:19 -0700

    Side-scrolling Camera Implemented

---
+++ Assets/CameraControl.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var threshold = 1;
+var player : GameObject;
+
+function Start () {
+	player = GameObject.FindGameObjectWithTag("Player");
+}
+
+function Update () {
+	if (Mathf.Abs(player.transform.position.x - transform.position.x)  > threshold) {
+		transform.position.x = Mathf.Lerp(transform.position.x, player.transform.position.x, 0.03);
+	}
+}
