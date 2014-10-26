f7d6a19a61c9465a9b825d99e1972475692d1b29
Xin-Yu-Tan <xinyutan@berkeley.edu>
2014-10-25 23:42:52 -0700

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit dd5c1c11cb98c921515b16352e559497b5358631.

--- Assets/MoveControls.js
+++ Assets/MoveControls.js
@@ -1,8 +1,8 @@
 ﻿#pragma strict
 
-var horzSpeed = 0.5;
-var vertSpeed = 7.5;
-var allowMultipleJumps = false;
+var horz_speed = 0.5;
+var vert_speed = 7.5;
+var allow_multiple_jumps = false;
 
 private var canJump;
 
@@ -14,15 +14,16 @@ function Start () {
 
 function Update () {
 	if (Input.GetKey(KeyCode.LeftArrow))
-		this.rigidbody2D.velocity.x -= horzSpeed;
+		this.rigidbody2D.velocity.x -= horz_speed;
 	if (Input.GetKey(KeyCode.RightArrow))
-		this.rigidbody2D.velocity.x += horzSpeed;
-	if (Input.GetKeyDown(KeyCode.DownArrow))
-		rigidbody2D.velocity.y -= vertSpeed;
+		this.rigidbody2D.velocity.x += horz_speed;
 	if (Input.GetKeyDown(KeyCode.UpArrow) && canJump){
-		rigidbody2D.velocity.y += vertSpeed;
-		if(!allowMultipleJumps)
+		rigidbody2D.velocity.y += vert_speed;
+		if(!allow_multiple_jumps)
 			canJump = false;
+	}
+	if (Input.GetKeyDown(KeyCode.DownArrow)){
+		rigidbody2D.velocity.y -= vert_speed;
 	}
 	
 	if (Input.GetKeyUp(KeyCode.LeftArrow))
