efa247c4961034dcfef1a0b0c67bf7709fa53467
Xin-Yu-Tan <xinyutan@berkeley.edu>
2014-10-28 15:58:26 -0700

    Half-fixed moving and jumping
    Moving now happens at a constant velocity
    You cannot perform any actions in mid-air

--- Assets/MoveControls.js
+++ Assets/MoveControls.js
@@ -1,29 +1,29 @@
 ﻿#pragma strict
 
-var horz_speed = 0.4;
+var horz_speed = 10;
 var vert_speed = 7.5;
-var allow_multiple_jumps = false;
+//var allow_multiple_jumps = false;
 
-private var canJump;
+private var isGrounded;
 
 
 function Start () {
 	this.rigidbody2D.fixedAngle = true;
-	canJump = true;
 }
 
 function Update () {
-	if (Input.GetKey(KeyCode.LeftArrow))
-		this.rigidbody2D.velocity.x -= horz_speed;
-	if (Input.GetKey(KeyCode.RightArrow))
-		this.rigidbody2D.velocity.x += horz_speed;
-	if (Input.GetKeyDown(KeyCode.UpArrow) && canJump){
-		rigidbody2D.velocity.y += vert_speed;
-		if(!allow_multiple_jumps)
-			canJump = false;
-	}
-	if (Input.GetKeyDown(KeyCode.DownArrow)){
-		rigidbody2D.velocity.y -= vert_speed;
+	if (isGrounded) {
+		if (Input.GetKey(KeyCode.LeftArrow))
+			this.rigidbody2D.velocity.x = -horz_speed;
+		if (Input.GetKey(KeyCode.RightArrow))
+			this.rigidbody2D.velocity.x = horz_speed;
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+			rigidbody2D.velocity.y -= vert_speed;
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			rigidbody2D.velocity.y += vert_speed;
+//			if(!allow_multiple_jumps)
+//				isGrounded = false;
+		}
 	}
 	
 	if (Input.GetKeyUp(KeyCode.LeftArrow))
@@ -33,9 +33,11 @@ function Update () {
 }
 
 function OnCollisionEnter2D(coll: Collision2D) {
-	if (coll.gameObject.tag == "Wall") {
-	}
-	if(coll.gameObject.tag == "Ground"){ 	// If the player lands on ground
-	    canJump = true; 					// allow him to jump again
-	}
+	if(coll.gameObject.tag == "Ground")
+	    isGrounded = true;
+}
+
+function OnCollisionExit2D(coll : Collision2D){
+	if(coll.gameObject.name == "Ground")
+		isGrounded = false;
 }
