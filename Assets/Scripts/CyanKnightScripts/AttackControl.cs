1d83012cbeca6acf3d0c894922e482035f8a7d03
KirbiKaka <calvinlu@berkeley.edu>
2014-11-12 23:48:25 -0800

    First weak attack animation

--- Assets/Scripts/CyanKnightScripts/AttackControl.cs
+++ Assets/Scripts/CyanKnightScripts/AttackControl.cs
@@ -1,26 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AttackControl : MonoBehaviour {
+public class AttackControl : MonoBehaviour
+{
 
-	public enum AttackState {None, Windup, Strike, Backswing}
+	public enum AttackState
+	{
+		None,
+		Windup,
+		Strike,
+		Backswing
+	}
+	protected Animator animator;
+	GameObject weak1StrikeBox;
+	GameObject curr_weak1StrikeBox;
 
 	/* previous attack tracker for combos */
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+		weak1StrikeBox = new GameObject ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+	{
+		Debug.Log ("naaas");
+		if (animator) {
+			Debug.Log ("yaas");
+			if (this.animator.GetCurrentAnimatorStateInfo (0).IsName ("CK_weak1")) {
+				return;
+			}
+			animator.SetInteger ("state", 0);
+			if (animator.GetInteger ("state") == 0) {
+				if (Input.GetKeyDown (KeyCode.Space)) {
+					WeakAttack ();
+				}
+			}
+		}
+	} 
 
-	void WeakAttack() {
+	void WeakAttack ()
+	{
 		// move the character forward with a small force
 		// start the attack animation
 		// update the states
-
+		animator.SetInteger ("state", 1);
+		curr_weak1StrikeBox = (GameObject)Instantiate (weak1StrikeBox,
+		                                   new Vector3 (transform.position.x + 1, transform.position.y, 0), transform.rotation);
+		curr_weak1StrikeBox.AddComponent<BoxCollider2D> ();
+		curr_weak1StrikeBox.GetComponent<BoxCollider2D> ().isTrigger = true;
+		//curr_weak1StrikeBox.AddComponent<TestCollide> ();
 	}
 }
