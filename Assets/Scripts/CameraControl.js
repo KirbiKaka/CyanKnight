93ba36c7074d031271bac22785f2020a2862a3f0
KirbiKaka <calvinlu@berkeley.edu>
2014-11-23 19:36:42 -0800

    Sprite size fix and camera fix

--- Assets/Scripts/CameraControl.js
+++ Assets/Scripts/CameraControl.js
@@ -1,14 +1,12 @@
 ﻿#pragma strict
 
 var threshold = 1;
-var player : GameObject;
 
 function Start () {
-	player = GameObject.FindGameObjectWithTag("Player");
 }
 
 function Update () {
-	if (Mathf.Abs(player.transform.position.x - transform.position.x)  > threshold) {
-		transform.position.x = Mathf.Lerp(transform.position.x, player.transform.position.x, 0.03);
+	if (Mathf.Abs(this.transform.position.x - Camera.main.transform.position.x)  > threshold) {
+		Camera.main.transform.position.x = Mathf.Lerp(Camera.main.transform.position.x, this.transform.position.x, 0.03);
 	}
 }
